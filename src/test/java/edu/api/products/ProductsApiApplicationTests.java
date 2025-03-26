ca97ae90254229cc793358c3363fc2edd01ca9c3
Leonardo Herrera <leonardoherrera2304@gmail.com>
2025-03-25 23:26:57 -0400

    first commit

---
+++ src/test/java/edu/api/products/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.api.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
